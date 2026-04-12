cf066d137c1cf23ad46c31d6153be09cfdf4eab6
Adwita8 <kommidiadwitareddy2008@gmail.com>
2026-04-12 17:39:43 +0530

    Added ChainlinkVRF feature

--- contracts/victoryVerse.sol
+++ contracts/victoryVerse.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts@1.3.0/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts@1.3.0/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract EventManager is ERC721URIStorage, Ownable {
+
+contract EventManager is ERC721URIStorage, VRFConsumerBaseV2Plus {
     uint256 public eventCount;
     uint256 public tokenIdCounter;
 
@@ -17,6 +20,9 @@ contract EventManager is ERC721URIStorage, Ownable {
     event ParticipantRegistered(uint256 indexed eventId, address indexed participant);
     event WinnerDeclared(uint256 indexed eventId, address indexed winner, uint256 tokenId);
     event FanTokensPurchased(uint256 indexed eventId, address indexed buyer, uint256 amount, uint256 cost);
+    event RandomWordRequested(uint256 indexed requestId, uint256 indexed eventId);
+    event RandomWordFulfilled(uint256 indexed requestId, uint256 indexed winnerIndex);
+    event VRFResultCancelled(uint256 indexed requestId, uint256 indexed eventId);
 
     struct EventInfo {
         uint256 id;
@@ -30,12 +36,28 @@ contract EventManager is ERC721URIStorage, Ownable {
         uint256 fanTokenPrice; // in wei
         address fanTokenAddress;
         string description;
+        bool useVRF;
     }
+
+    uint256 public s_subscriptionId;
+    address public vrfCoordinator;
+    bytes32 public keyHash;
+    uint32 public callbackGasLimit = 40000;
+    uint16 public requestConfirmations = 3;
+    uint32 public numWords = 1;
 
     mapping(uint256 => EventInfo) public events;
     mapping(uint256 => address[]) public eventParticipants;
+    mapping(uint256 => uint256) public Requestors;
+    mapping(uint256 => uint256) public Results;
+    mapping(uint256 => uint256) public requestIdToBlockNumber;
 
-    constructor() ERC721("EventNFT", "EVNT") Ownable(msg.sender) {}
+    constructor(address _vrfCoordinator, uint256 subscriptionId, bytes32 _keyHash) ERC721("EventNFT", "EVNT")
+    VRFConsumerBaseV2Plus(_vrfCoordinator){
+        s_subscriptionId = subscriptionId;
+        keyHash = _keyHash;
+        vrfCoordinator = _vrfCoordinator;
+    }
 
     function createEvent(
         string memory _name,
@@ -61,6 +83,7 @@ contract EventManager is ERC721URIStorage, Ownable {
         e.fanTokenAddress = address(token);
         e.meta_uri = _meta_uri;
         e.description = _description;
+        e.useVRF = false;
 
         emit EventCreated(eventCount, msg.sender, _name);
         return eventCount;
@@ -76,12 +99,109 @@ contract EventManager is ERC721URIStorage, Ownable {
         emit ParticipantRegistered(_eventId, msg.sender);
     }
 
+
+    function requestRandomWinner(uint256 _eventId) public returns (uint256 requestId) {
+        EventInfo storage e = events[_eventId];
+        require(msg.sender == e.creator, "Only event host can request winner");
+        require(!e.winnerDeclared, "Winner already declared");
+        require(eventParticipants[_eventId].length > 0, "No participants registered");
+
+        requestId = s_vrfCoordinator.requestRandomWords(
+        VRFV2PlusClient.RandomWordsRequest({
+            keyHash: keyHash,
+            subId: s_subscriptionId,
+            requestConfirmations: requestConfirmations,
+            callbackGasLimit: callbackGasLimit,
+            numWords: numWords,
+            extraArgs: VRFV2PlusClient._argsToBytes(
+                VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+            )
+        })
+     );
+
+       Requestors[requestId] = _eventId;
+       Results[requestId] = eventParticipants[_eventId].length;
+       requestIdToBlockNumber[requestId] = block.number;
+
+       emit RandomWordRequested(requestId, _eventId);
+
+    }
+
+    function fulfillRandomWords( uint256 requestId, uint256[] calldata randomWords) internal override {
+
+      EventInfo storage e = events[eventCount];
+
+     require(msg.sender == e.creator, "Not creator");
+     uint256 _eventId = Requestors[requestId];
+
+    if(eventParticipants[_eventId].length == 0) {
+
+        emit VRFResultCancelled(requestId, _eventId);
+        return;
+     }
+
+    if(block.number > requestIdToBlockNumber[requestId] + 256){
+        cancelVRFRequest(requestId);
+     }
+
+     uint256 winnerIndex = randomWords[0] % Results[requestId];
+     Results[requestId] = winnerIndex;
+
+     e.winner = eventParticipants[_eventId][winnerIndex];
+     e.useVRF = true;
+
+     emit RandomWordFulfilled(requestId, winnerIndex);
+     
+    }
+
+    function cancelVRFRequest(uint256 requestId) internal{
+
+        uint256 _eventId = Requestors[requestId];
+        EventInfo storage e = events[eventCount];
+
+        require(msg.sender == e.creator, "Only creator can cancel");
+        require(e.useVRF == true, "VRF not used");
+        require(block.number > requestIdToBlockNumber[requestId] + 256,"VRF request still pending");
+
+        delete Requestors[requestId];
+        delete Results[requestId];
+        delete requestIdToBlockNumber[requestId];
+
+        emit VRFResultCancelled(requestId, _eventId);
+
+        requestRandomWinner(_eventId);
+
+    }
+
+
+    function _declareWinner(uint256 _eventId) public {
+        EventInfo storage e = events[_eventId];
+        require(e.useVRF==true, "VRF not used");
+        require(msg.sender == e.creator, "Not creator");
+        require(!e.winnerDeclared, "Already declared");
+        require(isRegistered[_eventId][e.winner], "Winner not registered");
+        
+        e.winnerDeclared = true;
+        tokenIdCounter++;
+        _mint(e.winner, tokenIdCounter);
+        _setTokenURI(tokenIdCounter, e.meta_uri);
+        userNFTs[e.winner][_eventId] = tokenIdCounter;
+
+        EventToken token = EventToken(e.fanTokenAddress);
+        token.mint(e.winner, e.winnerTokenAmount);
+        token.mint(address(this), e.fanTokenAmount);
+        userFanTokenBalance[e.winner][_eventId] += e.winnerTokenAmount;
+
+        emit WinnerDeclared(_eventId, e.winner, tokenIdCounter);
+    }
+
     function declareWinner(uint256 _eventId, address _winner) public {
         EventInfo storage e = events[_eventId];
+        require(e.useVRF==false, "VRF is used");
         require(msg.sender == e.creator, "Not creator");
         require(!e.winnerDeclared, "Already declared");
         require(isRegistered[_eventId][_winner], "Winner not registered");
-
+        
         e.winner = _winner;
         e.winnerDeclared = true;
 
